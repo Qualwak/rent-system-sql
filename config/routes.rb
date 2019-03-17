5bf8aab4d57f9a1e5d5ff64e15ec9a55ac2eb91d
reshreshus <reshreshus@gmail.com>
2019-03-17 14:16:58 +0300

    write data generator, generate data

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :items
   resources :users
+  resources :items
+  resources :orders
   resources :subcategories
   resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,39 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-users_count = 10
-items_count = 10
-orders_count = 8
+users_count = 30
+
+category = Category.create({
+	title: "Test Category",
+	description: "BlahBluah Category"
+})
+subcategory = Subcategory.create({
+	title: "Test Subcategory",
+	description: "description of a SUBbcategory",
+	category: category
+})
 
 users_count.times do
-	User.Create({
+	user = User.create({
 		#Faker::Internet.password(10, 20) #=> "EoC9ShWd1hWq4vBgFw",
 		name: Faker::Name.first_name,
 		surname: Faker::Name.last_name,
 		username: Faker::Internet.username(5..8),
 		phone: Faker::PhoneNumber.phone_number_with_country_code,
-		email: Faker::Internet.emai,
+		email: Faker::Internet.email,
 		role_id: 1
 	})
-
-
-Category.Create({
-	title: "Test Category"
-	description: "BlahBluah Category"
-})
-
-Subcategory.Create({
-	title: "Test Subcategory"
-	description: "description of a SUBbcategory"
-	#category: reference here?
-})
-items_count.times do
-	Item.Create({
+	Item.create({
 		title: Faker::Device.model_name,
-		description: Faker::Lorem.sentenc,
-		price: Faker::Number.decimal(2, 3) #=> "18.843",
-		duration: 1 + rand(7)
+		description: Faker::Lorem.sentence,
+		price: Faker::Number.decimal(2, 3), #=> "18.843",
+		duration: 1 + rand(7),	
+		subcategory: subcategory,
+		user: user
 	})
 end
-
 
