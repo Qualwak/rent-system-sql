3ea95892cf4eb34b6c7122ab65d05ca5ff6bb6c8
Qualwak <darklihamb@gmail.com>
2019-03-24 19:42:49 +0300

    changed path

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :subcategories
   resources :categories
   resources :application
-  get 'current_user', to: 'users#current_user'
+  get 'current-user', to: 'users#current_user'
   get '/users/:id/items', to: 'users#get_user_items'
   get '/categories/:id/subcategories', to: 'categories#get_subcategories'
   post 'authenticate', to: 'authentication#authenticate'
